a3f669bd67d55ca1230226ade3d781687c164c2e
aloha-inc <naoharuyoshikawa@gmail.com>
2018-08-20 19:15:53 +0900

    acts-as-taggable-on使用してタグ登録機能の実装

--- config/routes.rb
+++ config/routes.rb
@@ -1,4 +1,5 @@
 Rails.application.routes.draw do
+  resources :schools
   get 'brands/index'
   get 'brands/show'
   # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
--- db/schema.rb
+++ db/schema.rb
@@ -10,13 +10,19 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 2018_08_20_084562) do
+ActiveRecord::Schema.define(version: 2018_08_20_084816) do
 
   create_table "brands", force: :cascade do |t|
     t.string "name"
     t.string "lesson"
     t.string "target"
     t.integer "hourly_rate"
+    t.datetime "created_at", null: false
+    t.datetime "updated_at", null: false
+  end
+
+  create_table "schools", force: :cascade do |t|
+    t.string "name"
     t.datetime "created_at", null: false
     t.datetime "updated_at", null: false
   end
