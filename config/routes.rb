0f026fe6b8661794a30e3efa6baf5a17ed7c1da7
aloha-inc <naoharuyoshikawa@gmail.com>
2018-08-20 17:23:57 +0900

    slim導入＆テスト

--- config/routes.rb
+++ config/routes.rb
@@ -1,3 +1,5 @@
 Rails.application.routes.draw do
+  get 'brands/index'
+  get 'brands/show'
   # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
 end
