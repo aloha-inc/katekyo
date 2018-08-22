d83997c77fb1ba257cfe53657e288a3974a1f1c2
aloha-inc <naoharuyoshikawa@gmail.com>
2018-08-22 17:15:22 +0900

    school新規登録フォームにbootstrap適用

--- db/seeds.rb
+++ db/seeds.rb
@@ -5,3 +5,43 @@
 #
 #   movies = Movie.create([{ name: 'Star Wars' }, { name: 'Lord of the Rings' }])
 #   Character.create(name: 'Luke', movie: movies.first)
+
+  School.create(name: 'TECH D') 
+
+  Purpose.create([
+    { name: '就職・転職' },
+    { name: 'スキルアップ' },
+    { name: '起業' },
+    { name: 'オリジナルアプリ作成' },
+    { name: 'A' },
+    { name: 'B' }
+  ])
+
+  Style.create([
+    { name: '教室' },
+    { name: 'オンライン' },
+    { name: '両方あり' },
+    { name: 'AA' },
+    { name: 'BB' },
+    { name: 'CC' }
+  ])
+
+  Status.create([
+    { name: '小学生' },
+    { name: '学生' },
+    { name: '社会人' },
+    { name: 'AAA' },
+    { name: 'BBB' },
+    { name: 'CCC' }
+  ])
+
+  Course.create([
+    { name: 'Ruby' },
+    { name: 'PHP' },
+    { name: 'Java' },
+    { name: 'ブロックチェーン' },
+    { name: 'Webデザイン' },
+    { name: 'Webアプリケーション作成' }
+  ])
+
+ 
